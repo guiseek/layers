66e18df6880d4fb0bc8a76b4e920d58639196816
Gui Seek <guiseek@gmail.com>
2022-05-15 03:13:43 -0300

    feat: graphql working

---
+++ apps/server/graphql/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { upperDirectiveTransformer } from './common/directives/upper-case.directive';
+import { CatsModule } from './cats/cats.module'
+
+@Module({
+  imports: [
+    CatsModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      transformSchema: schema => upperDirectiveTransformer(schema, 'upper'),
+      installSubscriptionHandlers: true,
+    }),
+  ],
+})
+export class AppModule {}
--- apps/server/graphql/src/main.ts
+++ apps/server/graphql/src/main.ts
@@ -1,22 +1,13 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe())
+
+  await app.listen(process.env.PORT)
+  console.log(`🚀 Running on: ${await app.getUrl()}`)
 }
 
-bootstrap();
+bootstrap()
