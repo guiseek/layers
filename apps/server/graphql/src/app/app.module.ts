fea32b072ed4f9255abe3dac86605783d0bdf1e2
Gui Seek <guiseek@gmail.com>
2022-05-15 01:14:53 -0300

    feat: graphql config

--- apps/server/graphql/src/app/app.module.ts
+++ apps/server/graphql/src/app/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { join } from 'path'
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+      autoSchemaFile: true,
+      playground: true,
+      debug: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
